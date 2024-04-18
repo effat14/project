a77e207defc337db288af1648c2970b05cc997dd
effat <joyislam1954@gmail.com>
2024-04-18 20:18:27 +0600

    models create

--- ecom/admin.py
+++ ecom/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Medicine)
+admin.site.register(Orders)
+admin.site.register(OrderItem)
--- ecom/models.py
+++ ecom/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pic/CustomerProfilePic/', null=True, blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def get_name(self):
+        return str(self.user.first_name) + " " + str(self.user.last_name)
+
+    @property
+    def get_id(self):
+        return str(self.user.id)
+
+    def __str__(self):
+        return str(self.user.first_name)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    product_image = models.ImageField(upload_to='medicine_image/', null=True, blank=True)
+    description = models.CharField(max_length=40)
+    cost = models.IntegerField()
+    selling_price = models.IntegerField()
+    qty = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Order Confirmed', 'Order Confirmed'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    email = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=500, null=True)
+    mobile = models.CharField(max_length=20, null=True)
+    order_date = models.DateField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=50, null=True, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.order
+
